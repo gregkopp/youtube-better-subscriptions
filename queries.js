9e40e24a75d679553f190dcc2e247bc5bf314324
Greg Kopp <greg@gregkopp.com>
2025-07-18 23:50:55 -0400

    Remove shorts header

--- queries.js
+++ queries.js
@@ -3,12 +3,12 @@ function vidQuery() {
         `ytd-grid-video-renderer.style-scope.ytd-grid-renderer:not(.${HIDDEN_CLASS})`,
         `ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer:not(.${HIDDEN_CLASS})`,
         `ytd-rich-item-renderer.style-scope.ytd-rich-grid-row:not(.${HIDDEN_CLASS})`,
-        `ytd-rich-item-renderer.style-scope.ytd-rich-shelf-renderer:not([is-post]):not(.${HIDDEN_CLASS})`
-    ].join(',');
+        `ytd-rich-item-renderer.style-scope.ytd-rich-shelf-renderer:not([is-post]):not(.${HIDDEN_CLASS})`,
+    ].join(",");
 }
 
 function sectionsQuery() {
-    return "ytd-item-section-renderer.style-scope.ytd-section-list-renderer";
+    return "ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer";
 }
 
 function sectionTitleQuery() {
